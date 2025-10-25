1f5a1fa950a8096f76fc22e7fa4b5b2af7e397d0
Zaryab Ahmad <zaryabahmad454@gmail.com>
2025-10-26 00:02:40 +0500

    update build folder

--- campus-chat/frontend/next.config.ts
+++ campus-chat/frontend/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
